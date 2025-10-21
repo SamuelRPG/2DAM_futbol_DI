026842c28f8da5458d23348ef37b719d83091932
Manel Viel <mviel@florida-uni.es>
2025-10-21 15:12:37 +0200

    modifique pantalla principal

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
-export default function App() {
+const App = () => {
+
+  //Açò é snecessari, per a gestionar la rotació. 
+  //Caldrà afegir-ho en tots els nostres projectes de classe.
+  useEffect(() => {
+    const unlockScreenOerientation = async () => {
+      await ScreenOrientation.unlockAsync()
+    }
+    unlockScreenOerientation()
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titol}>Llistat de partits:</Text>
       <StatusBar style="auto" />
+      <View style={{flex:0.25}}>
+        <InfoEquip nom="Valencia C.F." />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
+  titol: {
+    fontSize: 25,
+    fontWeight: "bold",
+
+  }
 });
+export default App;
