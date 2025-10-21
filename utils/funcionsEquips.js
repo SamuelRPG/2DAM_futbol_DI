27c7fdc7cf59a2070ecf390fd7c9bcd7ed97cd8a
Manel Viel <mviel@florida-uni.es>
2025-10-21 15:07:56 +0200

    funcionsEquips

---
+++ utils/funcionsEquips.js
@@ -0,0 +1,39 @@
+
+import * as file from '../assets/dades/dades.json' with { type: "json" };
+const dades = file.default.equips;//dades és un array d'equips, ara
+
+// A partir d'un nom d'un equip ('Valencia', 'Levante', 'Barcelona', ... ), 
+// obtinc l'objecte d'eixe equip amb totes seues dades.
+const getObjEquip = (nomEquip)=> {
+    //imprementeu-ho
+}
+
+//a partir d'un obj amb les dades d'un equip, obtinc la imatge (strTeamShort+"png")
+const getFileNameImatgeEscut = (unEquipObj)=>{
+    //implementeu-ho
+}
+
+//obtinc l'any de fundació (intFormedYear)
+const getAnyFundacio = (unEquipObj)=>{
+    //implementeu-ho
+}
+
+//obtinc l'any de fundació (intFormedYear)
+const getNomEstadi = (unEquipObj)=>{
+    //implementeu-ho
+}
+
+//obtinc el nom de l'equip (strTeam)
+const getNomEquip = (unEquipObj)=>{
+   //implementeu-ho
+}
+
+//obtinc la fitxa de l'equip.
+//La fitxa consisteix en un objecte amb els atributs: nom,imatgeEscut,anyFundacio,estadi
+const getFitxaEquip = (unEquipObj)=>{
+   //implementeu-ho
+}
+
+const equipACercar = 'Girona';
+
+
