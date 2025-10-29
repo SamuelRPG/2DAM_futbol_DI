f9edf61c27383229a2c26b0c0ad3bffbcf714ace
Manel Viel <mviel@florida-uni.es>
2025-10-29 15:22:31 +0100

    Preparem el repo inicial

--- App.js
+++ App.js
@@ -1,18 +1,9 @@
 import {React, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import * as ScreenOrientation from 'expo-screen-orientation';
-
 const App = () => {
 
-  //Açò é snecessari, per a gestionar la rotació. 
-  //Caldrà afegir-ho en tots els nostres projectes de classe.
-  useEffect(() => {
-    const unlockScreenOerientation = async () => {
-      await ScreenOrientation.unlockAsync()
-    }
-    unlockScreenOerientation()
-  }, [])
+
 
 // Pantalla Principal
   return (
