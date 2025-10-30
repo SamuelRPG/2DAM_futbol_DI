00f92c721b1c5f687791f76630f5d1e9fe50a9d7
Samuel <samuel.sbase@gmail.com>
2025-10-30 16:40:28 +0100

    clase DI 30/10

--- components/infoEquips.js
+++ components/infoEquips.js
@@ -1,13 +1,44 @@
 import * as React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
 import {getFitxaEquip, getObjEquip} from '../utils/funcionsEquips';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-return (
-    <PaperProvider>
+const InfoEquips = (props) => {
+    let objetoEquipo = getObjEquip(props.nom);
+    let fitxa = getFitxaEquip(objetoEquipo);
+
+    return (
         <View style={styles.container}>
+            <View style={styles.contenedorPartidos}>
+                <Image source={{uri: fitxa.imagenLogo}} style={{width: 100, height: 100}}/>
+                <Text> {fitxa.nombre} </Text>
+                <Text> {fitxa.anyFundacion}</Text>
+            </View>
+            <View style={styles.contenedorPartidos}>
 
+            </View>
         </View>
-    </PaperProvider>
-)
+    );
 
-//seguri
+}
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    margin:5,
+    padding: 5,
+    backgroundColor: "#fff",
+  },
+  contenedorPartidos: {
+    marginTop: 30,
+    justifyContent: "center", 
+    alignContent: "center",
+    padding: 15,
+    alignItems: "center",
+    borderColor: "black", 
+    borderWidth: 2,
+    borderRadius: 15, 
+    backgroundColor: "yellow",
+  },
+})
+
+export default InfoEquips;
--- utils/funcionsEquips.js
+++ utils/funcionsEquips.js
@@ -1,10 +1,9 @@
-
 import * as file from '../assets/dades/dades.json' with { type: "json" };
 const dades = file.default.equips;//dades és un array d'equips, ara
 
 // A partir d'un nom d'un equip ('Valencia', 'Levante', 'Barcelona', ... ), 
 // obtinc l'objecte d'eixe equip amb totes seues dades.
-const getObjEquip = (nomEquip) => {
+export const getObjEquip = (nomEquip) => {
  return dades.filter((elem) => elem.strTeam === nomEquip)[0];
 }
 
@@ -30,17 +29,16 @@ const getNomEquip = (unEquipObj)=>{
 
 //obtinc la fitxa de l'equip.
 //La fitxa consisteix en un objecte amb els atributs: nom,imatgeEscut,anyFundacio,estadi
-const getFitxaEquip = (unEquipObj)=>{
-   let objDevuelto = { nombre: unEquipObj.strTeam, imagenLogo: unEquipObj.strLogo, anyFundacion: unEquipObj.intFormedYear, estadio: unEquipObj.strStadium};
+export const getFitxaEquip = (unEquipObj)=>{
+   let objDevuelto = { nombre: unEquipObj.strTeam, imagenLogo: unEquipObj.strBadge, anyFundacion: unEquipObj.intFormedYear, estadio: unEquipObj.strStadium};
    return objDevuelto;
 }
 
-const equipACercar = 'Girona';
-let objetoEquipo = (getObjEquip(equipACercar));
-console.log(getObjEquip(equipACercar));
-console.log(getUrlImatgeEscut(objetoEquipo));
-console.log(getAnyFundacio(objetoEquipo));
-console.log(getNomEstadi(objetoEquipo));
-console.log(getNomEquip(objetoEquipo));
-console.log(getFitxaEquip(objetoEquipo));
-
+// const equipACercar = 'Girona';
+// let objetoEquipo = (getObjEquip(equipACercar));
+// console.log(getObjEquip(equipACercar));
+// console.log(getUrlImatgeEscut(objetoEquipo));
+// console.log(getAnyFundacio(objetoEquipo));
+// console.log(getNomEstadi(objetoEquipo));
+// console.log(getNomEquip(objetoEquipo));
+// console.log(getFitxaEquip(objetoEquipo));
